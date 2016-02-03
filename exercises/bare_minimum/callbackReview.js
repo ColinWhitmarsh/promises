e7ac77d952c226dc19eab0dc22222468d2190712
Colin Whitmarsh <colin.whitmarsh@gmail.com>
2016-02-03 11:37:14 -0800

    pass pluckFirstLineFromFile tests

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -36,9 +36,16 @@ var request = require('request');
 // This function should retrieve the first line of the file at `filePath`
 // HINT: Passing 'utf8' as the second argument to fs.readFile will give you a stringified file
 // HINT: You can get an array of lines by splitting on the '\n' character
-var pluckFirstLineFromFile = function (filePath
+var pluckFirstLineFromFile = function (filePath, callback
 ) {
-  // YOUR CODE HERE
+  fs.readFile(filePath, 'utf8', function(err, content) {
+    if(err) {
+      callback(err)
+    } else {
+      var firstLine = content.split("\n")[0]
+      callback(err, firstLine);
+    }
+  })
 };
 
 // This function should retrieve the status code of a GET request to `url`
